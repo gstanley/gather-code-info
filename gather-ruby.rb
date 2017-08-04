9b820f6c3d752e1ad05fd921c6a39c8807a6c678
Greg Stanley <gregory.stanley@optum.com>
2017-08-04 12:05:58 -0600

    parse handle multiple constructs

--- gather-ruby.rb
+++ gather-ruby.rb
@@ -1,17 +1,20 @@
 require 'parser/current'
 require 'unparser'
 require 'find'
+require 'pry'
 
 PROJECT_DIR = "~/Developer/sms-manager-develop"
 
 def gather_project(dir = PROJECT_DIR)
   result = {files: []}
-  
+
   Find.find(File.expand_path(dir)) do |name|
     if !FileTest.directory?(name)
       if File.extname(name) == ".rb" || File.extname(name) == ".rabl" || File.extname(name) == ".rake"
         source = File.read(name)
-        result[:files] << {name: name, source: source}
+        result[:files] << {name: name,
+                           source: source,
+                           top: gather_ruby_source(source)}
       end
     end
   end
@@ -20,4 +23,24 @@ def gather_project(dir = PROJECT_DIR)
 end
 
 def gather_ruby_source(source)
+  binding.pry
+  result = {}
+  ast, comments = Parser::CurrentRuby.parse_with_comments(source)
+  result[:ast] = ast
+  result[:comments] = comments
+  result[:children] = []
+  if ast.type == :begin
+    elements = ast.children
+  else
+    elements = [ast]
+  end
+  elements.each do |element|
+    result[:children] << gather_ruby_construct(element)
+  end
+
+  result
+end
+
+def gather_ruby_construct(node)
+  nil
 end
